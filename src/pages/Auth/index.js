3aa44f01a471e7ebf53a707683cb934aa3fd815c
Putra Nurfajar <putranf@gmail.com>
2021-04-28 23:30:15 +0700

    [FEATURE] Auth

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Button, Card, Row, Container, Form } from "react-bootstrap";
 import LogoPP from "../../assets/images/pemudapeduli.png";
+import { useForm } from "react-hook-form";
 
-const index = () => {
+function Index() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);  
+
   return (
     <div>
       <Container>
@@ -12,18 +16,26 @@ const index = () => {
               <Card.Img variant="top" src={LogoPP} style={{ width: "10rem" }} />
             </Row>
             <Card.Body>
-              <Form>
+              <Form onSubmit={handleSubmit(onSubmit)}>
                 <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                  <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                    placeholder="Enter Username"                    
+                    {...register("username", {
+                      required: true,                     
+                    })}
+                  />
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
+                  <Form.Control
+                    placeholder="Enter Password"
+                    type="password"                    
+                    {...register("password", {
+                      required: true,                      
+                    })}
+                  />
                 </Form.Group>
                 <Form.Group controlId="formBasicCheckbox">
                   {/* <Form.Check type="checkbox" label="Check me out" /> */}
@@ -31,13 +43,24 @@ const index = () => {
                 <Button variant="primary" type="submit" block>
                   Login
                 </Button>
+                <hr />
+                <Form.Text>
+                  <center>Belum punya Akun ? Daftar</center>
+                </Form.Text>
+                <hr />
               </Form>
+              <Button variant="primary" href="https://kitabisa.com" target="_blank" block>
+                Donasi via KitaBisa.com
+              </Button>
+              <Button variant="primary" href="https://ayobantuin.com" target="_blank" block>
+                Donasi via AyoBantuin.com
+              </Button>
             </Card.Body>
           </Card>
         </Row>
       </Container>
     </div>
   );
-};
+}
 
-export default index;
+export default Index;
